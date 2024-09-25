92358de660eec7d3e240f819f7bc65cecc682d45
khollyli <94080024+khollyli@users.noreply.github.com>
2024-09-25 13:04:44 -0400

    Change bear img

--- Bear.java
+++ Bear.java
@@ -7,22 +7,47 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Bear extends Actor
 {
+    GreenfootImage[] idleRight = new GreenfootImage[4];
+    GreenfootImage[] idleLeft = new GreenfootImage[4];
+    
+    // Direction the elephant is facing
+    String facing = "right";
+
     /**
-     * Act - do whatever the Seal wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
+     * Constructor - The code that gets run one time when object is created
      */
+    public Bear()
+    {
+        for(int i = 0; i < idleRight.length; i++)
+        {
+            idleRight[i] = new GreenfootImage("images/bear_idle/idle" + i + ".png");
+            idleRight[i].scale(100, 100);
+        }
+        
+        for(int i = 0; i < idleLeft.length; i++)
+        {
+            idleLeft[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+            idleLeft[i].mirrorHorizontally();
+            idleLeft[i].scale(100, 100);            
+        }
+        
+        setImage(idleRight[0]);
+    }
+    
     public void act()
     {
         if(Greenfoot.isKeyDown("left"))
         {
             move(-3);
+            facing = "left";
         }
         else if(Greenfoot.isKeyDown("right"))
         {
             move(3);
+            facing = "right";
         }
         
-        //Remove apple if seal eats it
+        //Remove apple if bear eats it
         eatFood();
     }
     /**
