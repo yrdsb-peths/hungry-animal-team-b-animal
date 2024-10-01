d8a3430583ddb74061fa006776f9b3634b9565f1
scarlettho <159584626+scarlettho@users.noreply.github.com>
2024-09-30 20:17:09 -0400

    Update Bear.java

--- Bear.java
+++ Bear.java
@@ -7,88 +7,92 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Bear extends Actor implements Destructable
 {
-GreenfootSound eatingSound = new GreenfootSound("eating-sound-effect-36186.mp3");
-GreenfootImage[] idleRight = new GreenfootImage[4];
-GreenfootImage[] idleLeft = new GreenfootImage[4];
-
-// Direction the elephant is facing
-String facing = "right";
-SimpleTimer animationTimer = new SimpleTimer();
-
-
-public Bear()
-{
-    for(int i = 0; i < idleRight.length; i++)
-    {
-        idleRight[i] = new GreenfootImage("images/bear_idle/idle" + i + ".png");
-        idleRight[i].scale(100, 100);
-    }
+    GreenfootSound eatingSound = new GreenfootSound("eating-sound-effect-36186.mp3");
+    GreenfootImage[] idleRight = new GreenfootImage[4];
+    GreenfootImage[] idleLeft = new GreenfootImage[4];
     
-    for(int i = 0; i < idleLeft.length; i++)
+    // Direction the elephant is facing
+    String facing = "right";
+    SimpleTimer animationTimer = new SimpleTimer();
+    public Bear()
     {
-        idleLeft[i] = new GreenfootImage("images/bear_idle/idle" + i + ".png");
-        idleLeft[i].mirrorHorizontally();
-        idleLeft[i].scale(100, 100);            
-    }
-    
-    animationTimer.mark();
-    
-    // Initial bear image
-    setImage(idleRight[0]);
-}
-
-/**
- * Animate the bear
- */
-int imageIndex = 0;
-public void animateBear()
-{
-    if(animationTimer.millisElapsed() < 100)
-    {
-        return;
-    }
-    animationTimer.mark();
-    
-    if(facing.equals("right"))
-    {
-        setImage(idleRight[imageIndex]);
-        imageIndex = (imageIndex + 1) % idleRight.length;
-    }
-    else
-    {
-        setImage(idleLeft[imageIndex]);
-        imageIndex = (imageIndex + 1) % idleLeft.length;
-    }
-}
-    
-public void act()
-    {
-        if(Greenfoot.isKeyDown("left"))
+        for(int i = 0; i < idleRight.length; i++)
         {
-            move(-3);
-            facing = "left";
-        }
-        else if(Greenfoot.isKeyDown("right"))
-        {
-            move(3);
-            facing = "right";
+            idleRight[i] = new GreenfootImage("images/bear_idle/idle" + i + ".png");
+            idleRight[i].scale(100, 100);
         }
         
-        // Remove apple if bear eats it
-        destruct();
+        for(int i = 0; i < idleLeft.length; i++)
+        {
+            idleLeft[i] = new GreenfootImage("images/bear_idle/idle" + i + ".png");
+            idleLeft[i].mirrorHorizontally();
+            idleLeft[i].scale(100, 100);            
+        }
         
-        // Animate the bear
-        animateBear();
+        animationTimer.mark();
+        
+        // Initial bear image
+        setImage(idleRight[0]);
     }
-    public void destruct() {
-        if(isTouching(Food.class))
+    
+    /**
+     * Animate the bear
+     */
+    int imageIndex = 0;
+    public void animateBear()
+    {
+        if(animationTimer.millisElapsed() < 100)
+        {
+            return;
+        }
+        animationTimer.mark();
+        
+        if(facing.equals("right"))
+        {
+            setImage(idleRight[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleRight.length;
+        }
+        else
+        {
+            setImage(idleLeft[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleLeft.length;
+        }
+    } 
+    public void act()
+        {
+            if(Greenfoot.isKeyDown("left"))
             {
-                removeTouching(Food.class);
-                MyWorld world = (MyWorld)getWorld();
-                world.createFood();
-                world.increaseScore();
-                eatingSound.play();
+                move(-3);
+                facing = "left";
             }
+            else if(Greenfoot.isKeyDown("right"))
+            {
+                move(3);
+                facing = "right";
+            }
+            
+            // Remove apple if bear eats it
+            destruct();
+            
+            // Animate the bear
+            animateBear();
+        }
+    public void destruct() {
+        if(isTouching(Apple.class))
+        {
+            removeTouching(Food.class);
+            MyWorld world = (MyWorld)getWorld();
+            world.createFood();
+            world.increaseAppleScore();
+            eatingSound.play();
+        }
+        if(isTouching(Strawberry.class))
+        {
+            removeTouching(Food.class);
+            MyWorld world = (MyWorld)getWorld();
+            world.createFood();
+            world.increaseStrawberryScore();
+            eatingSound.play();
+        }
     }
-    
 }
